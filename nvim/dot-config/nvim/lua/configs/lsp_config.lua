39d9e2994779338cf66c76f97d4b82d5ee038095
Arne <dev@arne.to>
2026-04-29 18:20:06 +0200

    nvim: fix diagnositc signs

--- nvim/dot-config/nvim/lua/configs/lsp_config.lua
+++ nvim/dot-config/nvim/lua/configs/lsp_config.lua
@@ -100,14 +100,21 @@ lsp.enable('texlab')
 -- }
 
 
-local signs = {
-    Error = "󰅚 ",
-    Warn = " ",
-    Hint = "󰌶 ",
-    Info = " "
-}
-
-for type, icon in pairs(signs) do
-    local hl = "DiagnosticSign" .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
+vim.diagnostic.config({
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = '󰅚 ',
+            [vim.diagnostic.severity.WARN] = ' ',
+            [vim.diagnostic.severity.INFO] = ' ',
+            [vim.diagnostic.severity.HINT] = '󰌶 ',
+        },
+        linehl = {
+            -- [vim.diagnostic.severity.ERROR] = 'ErrorMsg',
+        },
+        numhl = {
+            [vim.diagnostic.severity.ERROR] = 'ErrorMsg',
+            [vim.diagnostic.severity.WARN] = 'WarningMsg',
+        },
+    },
+    severity_sort = true,
+})
